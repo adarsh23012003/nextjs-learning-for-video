ad5a6ad1b0cb9286d41aa6d3b914019251c82c06
Adarsh Maurya <adarshmaurya2303@gmail.com>
2022-12-23 15:52:41 +0530

    about page created for blog

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,52 @@ export default function About() {
       <Head>
         <title>About page</title>
       </Head>
-      <h1>About page</h1>
+      <div>
+        <h1 className='font-bold text-3xl center p-5'>About Hunting Coder</h1>
+        <div className='flex mx-40'>
+          <h2 className='font-bold text-2xl flex text-left pb-3'>
+            Introduction
+          </h2>
+        </div>
+        <p className='flex mx-40 text-left'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          dolor omnis? Quia cumque perferendis blanditiis voluptas consequatur
+          ut et modi ducimus. Ab ullam iure laudantium ipsum quisquam similique
+          consequuntur nemo reprehenderit. Odit consequuntur numquam eveniet
+          quaerat dignissimos a repellendus? Expedita enim doloremque aspernatur
+          iste dicta necessitatibus cupiditate totam error debitis.
+        </p>
+        <h2 className='font-bold text-2xl flex text-left pb-3 pt-3 mx-40'>
+          Services Provided
+        </h2>
+        <p className='flex mx-40 text-left'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+          autem repellat, ex nostrum fugiat commodi quos! Omnis neque veritatis
+          vero iste libero excepturi veniam recusandae. We offer the following
+          services
+        </p>
+        <ul className='mx-44 text-left list-disc'>
+          <li>Service1</li>
+          <li>Service2</li>
+          <li>Service3</li>
+          <li>Service4</li>
+          <li>Service5</li>
+        </ul>
+        <h2 className='font-bold text-2xl flex text-left pb-3 pt-3 mx-40'>
+          Contact us
+        </h2>
+        <p className='flex mx-40 text-left'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+          autem repellat, ex nostrum fugiat commodi quos! Omnis neque veritatis
+          vero iste libero excepturi veniam recusandae. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Dolorem, dolor omnis? Quia cumque
+          perferendis blanditiis voluptas consequatur ut et modi ducimus. Ab
+          ullam iure laudantium ipsum quisquam similique consequuntur nemo
+          reprehenderit. Odit consequuntur numquam eveniet quaerat dignissimos a
+          repellendus? Expedita enim doloremque aspernatur iste dicta
+          necessitatibus cupiditate totam error debitis.
+        </p>
+      </div>
     </>
   );
 }
