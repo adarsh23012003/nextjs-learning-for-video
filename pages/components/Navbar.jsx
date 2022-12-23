64fe828ba87af84fa9b3d5a09979953a62dfaeb6
Adarsh Maurya <adarshmaurya2303@gmail.com>
2022-12-23 15:51:55 +0530

    Navbar in one route added

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -3,13 +3,16 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className='bg-blue-400'>
-      <ul className='p-2 gap-3 justify-center flex'>
+    <nav className='font-bold p-10'>
+      <ul className='p-2 gap-12 justify-center flex'>
         <Link href='/'>
           <li className=' hover:text-blue-600'>Home</li>
         </Link>
         <Link href='/about'>
           <li className=' hover:text-blue-600'>About</li>
+        </Link>
+        <Link href='/blog'>
+          <li className=' hover:text-blue-600'>Blog</li>
         </Link>
         <Link href='/contact'>
           <li className=' hover:text-blue-600'>Contact</li>
